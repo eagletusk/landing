96657645b42772b6a6a2e751dbe5fe279eab79e3
Paul Sobczak <example@example.com>
2019-03-22 14:59:02 -0700

    Javascript working, added cleaned up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,83 +1,23 @@
 
 // Github XMLHttpRequest section
 var request = new XMLHttpRequest();
-var request2 = new XMLHttpRequest();
 var text = "";
-var i = 0;
 var index;
-var a = ["a", "b", "c"];
-var myObj, i, j, x = "";
-myObj = {
-  "name":"John",
-  "age":30,
-  "cars": [
-    {"name":"Ford", "models":["Fiesta", "Focus", "Mustang"]},
-    {"name":"BMW", "models":["320", "X3", "X5"]},
-    {"name":"Fiat", "models":["500", "Panda"] }
-  ]
-}
 
 function printRepoCount() {
     var responseObj = JSON.parse(this.responseText);
     var count = Object.keys(responseObj).length;
     for (index = 0; index <= count-1; ++index){
-      text = text +  " Repo "+ index +" name: "+ responseObj[index].name+ "<br>";
-
+      text = text +  "<li><em> Github Repository name: " + '<a href="' + responseObj[index].html_url + '">'+ responseObj[index].name + "</a>"+ "</em></li>";
     }
-    document.getElementById("github2").innerHTML = text;
+    document.getElementById("github").innerHTML = text;
   }
-
-  // document.getElementById("github1").innerHTML = responseObj[0].name + "a";
-  //
-  // document.getElementById("github2").innerHTML = "Username: " + responseObj.login+
-  // "<br> Name: " + responseObj.name +
-  // "<br> Company: " + responseObj.company +
-  // "<br> Blog: " + responseObj.blog +
-  // "<br> Location: " + responseObj.location +
-  // "<br> # of repos: " + responseObj.public_repos +
-  // "";
-
-
 
 //Git hub XMLHttpRequest
 var request = new XMLHttpRequest();
 request.onload = printRepoCount;
 request.open('get', 'https://api.github.com/users/eagletusk/repos', true)
 request.send()
-
-// function printRepoCount() {
-//   var responseObj = JSON.parse(this.responseText);
-//   console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
-// //  myHeading.textContent = "hello";
-//   document.getElementById("github1").innerHTML = responseObj.name + " has " + responseObj.public_repos + " public repositories!";
-//
-//   document.getElementById("github2").innerHTML = "Username: " + responseObj.login+
-//   "<br> Name: " + responseObj.name +
-//   "<br> Company: " + responseObj.company +
-//   "<br> Blog: " + responseObj.blog +
-//   "<br> Location: " + responseObj.location +
-//   "<br> # of repos: " + responseObj.public_repos +
-//   "";
-//
-// }
-//
-// function printRepos() {
-//   document.getElementById("github3").innerHTML = "3" ;
-//
-// }
-
-// //Git hub XMLHttpRequest
-// var request = new XMLHttpRequest();
-// request.onload = printRepoCount;
-// request.open('get', 'https://api.github.com/users/eagletusk/', true)
-// request.send()
-
-//Git hub XMLHttpRequest
-// var request2 = new XMLHttpRequest();
-// request.onload = printRepos;
-// request.open('get', 'https://api.github.com/users/eagletusk/repos', true)
-// //request.send()
-
 
 //Map code
 var L;
@@ -90,7 +30,3 @@ window.onload = function() {
     zoom: 12
   });
 }
-
-// "<br> URL: " + responseObj.url +
-// "<br> blog: " + responseObj.blog +
-// "<br> location: " + responseObj.location +
